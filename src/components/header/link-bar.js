db8857bf13255dfb1a9bbcaedbc8bcbeec47d5ff
Mike Hickey <59627140+hickem@users.noreply.github.com>
2020-11-08 10:39:18 -0500

    Update link-bar.js

--- src/components/header/link-bar.js
+++ src/components/header/link-bar.js
@@ -6,7 +6,7 @@ const LinkBar = () => {
 	return(
 		<div className="link-bar">
 			<div className="container">
-				<p>Visit <a href="https://www.skyltics.com" target="_blank" rel="noreferrer">Skylytics.com</a></p>
+				<p>Visit <a href="https://www.skylytics.com" target="_blank" rel="noreferrer">Skylytics.com</a></p>
 			</div>
 		</div>
 	)	
